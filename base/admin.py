ab12734f7dd24fad1c80a8be32df4dcdd6ba52ce
lily <lily176743@gmail.com>
2023-06-15 17:15:34 +1000

    Updated

---
+++ base/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(FollowersCount)
+admin.site.register(Comment)
+admin.site.register(LikePost)
+
+
+
